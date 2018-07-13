7f19eef6250ccb71f1789f626917a084d3b7c183
Ubaid Manzoor <deadhacker014@gmail.com>
2018-07-13 14:45:55 +0530

    Add it

--- Week5 Assiment/machine-learning-ex4/ex4/nnCostFunction.m
+++ Week5 Assiment/machine-learning-ex4/ex4/nnCostFunction.m
@@ -21,8 +21,8 @@ Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
 
 Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
                  num_labels, (hidden_layer_size + 1));
-disp(size(Theta1));
-disp(size(Theta2));
+%disp(size(Theta1));
+%disp(size(Theta2));
 % Setup some useful variables
 m = size(X, 1);
          
@@ -41,11 +41,11 @@ Theta2_grad = zeros(size(Theta2));
 %         computed in ex4.m
 %
 
-  a1 = [ones(m,1) X];
-
-  a2 = sigmoid(Theta1 * a1');
-  a2 = [ones(size(a2,2),1)';a2];
-  h = sigmoid(Theta2 * a2);
+  a1 = X;
+  a1_biased = [ones(m,1) X];
+  a2 = sigmoid(Theta1 * a1_biased');
+  a2_biased = [ones(size(a2,2),1)';a2];
+  h = sigmoid(Theta2 * a2_biased);
 
   yVec = eye(size(y,1),size(Theta2,1))(y,:);
 
@@ -71,37 +71,29 @@ Theta2_grad = zeros(size(Theta2));
 DELTA_one = 0;
 DELTA_two = 0;
 %UB = Unbiased
-UB_a1 = a1(:,2:end);
-UB_a2 = a2(2:end,:)';
-UB_Theta1 = Theta1(:,2:end);
+UB_a1 = a1_biased(:,2:end);
+UB_a2 = a2_biased(2:end,:)';
+B_a2_i = a2_biased;
+UB_Theta1 = Theta1;
 UB_Theta2 = Theta2(:,2:end);
-disp(size(UB_Theta1));
-disp(size(UB_Theta2));
-disp("->");
-disp(size(UB_a1));
-disp(size(UB_a2));
-disp("->");
+
 for i = 1:m,
-  UB_a1_i = UB_a1(i,:);%1 400
-  UB_a2_i = UB_a2(i,:);%1 25
+  UB_a2_i = UB_a2(i,:);%1 26
+  B_a2_i = a2_biased'(i,:);%1 25
+  B_a1_i = a1_biased(i,:);%1 401
   a3_i = h'(i,:);%1 10
   yVec_i = yVec(i,:);%1 10
-  
+
   delta3_i = (a3_i .- yVec_i)';%10 1
   
   g_2_i = UB_a2_i .* (1-UB_a2_i);%1 25
   delta2_i = (UB_Theta2' * delta3_i) .* g_2_i';% 25 10 * 10 1 .* 25 1-> 25 1
-  disp("->");
-  disp(size(delta2_i));
-  disp(size(g_2_i));
-  disp("->");
-  DELTA_one = DELTA_one + delta2_i * UB_a1_i;%  + 25 1 * 1 400
-  DELTA_two = DELTA_two + delta3_i * UB_a2_i;%  + 10 1 * 1 25
+
+  DELTA_one = DELTA_one + delta2_i * B_a1_i;%  + 25 1 * 1 401
+  DELTA_two = DELTA_two + delta3_i * B_a2_i;%  + 10 1 * 1 26
+
 end;
-Theta1_grad = DELTA_one/m + lambda .* UB_Theta1;
-Theta2_grad = DELTA_two/m + lambda .* UB_Theta2;
-disp(size(Theta1_grad));
-disp(size(Theta2_grad));
+
 % Part 3: Implement regularization with the cost function and gradients.
 %
 %         Hint: You can implement this around the code for
@@ -109,6 +101,10 @@ disp(size(Theta2_grad));
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
+Theta1_grad = DELTA_one/m + lambda/m .* UB_Theta1;
+Theta1_grad(:,1) = DELTA_one(:,1)/m;
+Theta2_grad = DELTA_two/m + lambda/m .* Theta2;
+Theta2_grad(:,1) = DELTA_two(:,1)/m;
 % -------------------------------------------------------------
 % =========================================================================
 
