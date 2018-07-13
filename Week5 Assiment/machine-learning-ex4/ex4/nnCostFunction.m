5b92ca0d7938a824d47f25b8844b5ec02fbcfcb5
Ubaid Manzoor <deadhacker014@gmail.com>
2018-07-13 12:14:13 +0530

    Just to save my work

--- Week5 Assiment/machine-learning-ex4/ex4/nnCostFunction.m
+++ Week5 Assiment/machine-learning-ex4/ex4/nnCostFunction.m
@@ -21,7 +21,8 @@ Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
 
 Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
                  num_labels, (hidden_layer_size + 1));
-
+disp(size(Theta1));
+disp(size(Theta2));
 % Setup some useful variables
 m = size(X, 1);
          
@@ -40,18 +41,18 @@ Theta2_grad = zeros(size(Theta2));
 %         computed in ex4.m
 %
 
-  X = [ones(m,1) X];
+  a1 = [ones(m,1) X];
 
-  hidden_layer = sigmoid(Theta1 * X');
-  hidden_layer = [ones(size(hidden_layer,2),1)';hidden_layer];
-  h = sigmoid(Theta2 * hidden_layer);
+  a2 = sigmoid(Theta1 * a1');
+  a2 = [ones(size(a2,2),1)';a2];
+  h = sigmoid(Theta2 * a2);
 
-  y = eye(size(y,1),size(Theta2,1))(y,:);
+  yVec = eye(size(y,1),size(Theta2,1))(y,:);
 
-  J = - (1/m) * sum(sum(y .* log(h)' + (1 - y) .* log(1 - h)')) + (lambda/(2*m)) ...
+  J = - (1/m) * sum(sum(yVec .* log(h)' + (1 - yVec) .* log(1 - h)')) + (lambda/(2*m)) ...
                                                   * (sum(sum(Theta1(:,2:end).^2)) ...
                                                   + sum(sum(Theta2(:,2:end).^2)));
-  disp((lambda/2*m) * (sum(sum(Theta1(:,2:end).^2))  + sum(sum(Theta2(:,2:end).^2))));
+  
 % Part 2: Implement the backpropagation algorithm to compute the gradients
 %         Theta1_grad and Theta2_grad. You should return the partial derivatives of
 %         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
@@ -67,6 +68,40 @@ Theta2_grad = zeros(size(Theta2));
 %               over the training examples if you are implementing it for the 
 %               first time.
 %
+DELTA_one = 0;
+DELTA_two = 0;
+%UB = Unbiased
+UB_a1 = a1(:,2:end);
+UB_a2 = a2(2:end,:)';
+UB_Theta1 = Theta1(:,2:end);
+UB_Theta2 = Theta2(:,2:end);
+disp(size(UB_Theta1));
+disp(size(UB_Theta2));
+disp("->");
+disp(size(UB_a1));
+disp(size(UB_a2));
+disp("->");
+for i = 1:m,
+  UB_a1_i = UB_a1(i,:);%1 400
+  UB_a2_i = UB_a2(i,:);%1 25
+  a3_i = h'(i,:);%1 10
+  yVec_i = yVec(i,:);%1 10
+  
+  delta3_i = (a3_i .- yVec_i)';%10 1
+  
+  g_2_i = UB_a2_i .* (1-UB_a2_i);%1 25
+  delta2_i = (UB_Theta2' * delta3_i) .* g_2_i';% 25 10 * 10 1 .* 25 1-> 25 1
+  disp("->");
+  disp(size(delta2_i));
+  disp(size(g_2_i));
+  disp("->");
+  DELTA_one = DELTA_one + delta2_i * UB_a1_i;%  + 25 1 * 1 400
+  DELTA_two = DELTA_two + delta3_i * UB_a2_i;%  + 10 1 * 1 25
+end;
+Theta1_grad = DELTA_one/m + lambda .* UB_Theta1;
+Theta2_grad = DELTA_two/m + lambda .* UB_Theta2;
+disp(size(Theta1_grad));
+disp(size(Theta2_grad));
 % Part 3: Implement regularization with the cost function and gradients.
 %
 %         Hint: You can implement this around the code for
@@ -74,27 +109,7 @@ Theta2_grad = zeros(size(Theta2));
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 % -------------------------------------------------------------
-
 % =========================================================================
 
 % Unroll gradients
